24723ff9a2b617b72f13300417108e41eca72a15
Joshua Jeth Manalili <shemerlo123@gmail.com>
2024-04-24 09:16:24 +0800

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hello World");
+
+let btnCreate = document.querySelector("#btnCreate");
+btnCreate.onclick = function () {
+    let floatingProductName = document.querySelector("#floatingProductName");
+    let floatingPrice = document.querySelector("#floatingPrice");
+    createProduct(floatingProductName.value, floatingPrice.value);
+};
+
+function createProduct(productName, productPrice){
+    if(!productName) return alert("Product Name is required");
+    else if (!productPrice) return alert(`Product must have a price.`);
+
+    let product = {
+        name: productName,
+        price: productPrice,
+    };
+
+    localStorage.setItem("product", JSON.stringify(product));
+
+    console.log("Creating product ...", productName, productPrice);
+}
